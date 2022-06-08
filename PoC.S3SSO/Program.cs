1a4b56a01b6efd674bda5de1a42b7a0c05430ecd
Manuel Cañete <manuel.canete@docplanner.com>
2022-06-08 19:14:03 +0200

    feat: add PoC to connect to S3 with an AWS SSO profile

---
+++ PoC.S3SSO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+var bucketName = args[0];
+var region = args[1];
+var key = args[2];
+
+var client = new AmazonS3Client(new AmazonS3Config
+{
+    RegionEndpoint = RegionEndpoint.GetBySystemName(region)
+});
+
+using var response = await client.GetObjectAsync(new GetObjectRequest
+{
+    BucketName = bucketName,
+    Key = key
+});
+
+if (response.HttpStatusCode == HttpStatusCode.OK)
+{
+    using var reader = new StreamReader(response.ResponseStream);
+    Console.WriteLine(await reader.ReadToEndAsync());
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# PoC.S3SSO
+
+## Prerequisites
+- Install AWSSDK.S3, AWSSDK.SSO and AWSSDK.SSOOIDC
+- Configure a default AWS profile to let AWS SDK use it for the credentials
+    - more information here -> https://docs.aws.amazon.com/sdk-for-net/v3/developer-guide/creds-assign.html
+
+## Running the application
+Modify the [launchsettings.json](./PoC.S3SSO/Properties/launchSettings.json) with your bucket, region and object key and... run the PoC.
